9a75967ea182db28c23e454adb470d1e472b168c
polendina <solicitmind@gmail.com>
2024-01-02 12:56:10 +0200

    refact: use floating-ui library for tooltip positioning - Using floating-ui library instead of vanilla javascript   manual positioning - Remove extraneous tippy.js snippets

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -1,58 +1,22 @@
 import './tippy'
 import {parseGoogleTranslateResponse} from "./translation/Translation";
-import tippy from "tippy.js";
 import {playAudio} from "./pronunciation/audio";
 import {retrieveRecordings} from "./pronunciation/forvo";
 import {sortRecordings} from "./pronunciation/utils";
 import {search} from "fast-fuzzy";
+import {autoUpdate, computePosition} from "@floating-ui/dom";
 
-const {fuzzy} = require("fast-fuzzy")
+let highlightedWord = ""
 
 function createPopoverContainer() {
     let popover = document.createElement('div');
-    popover.className = 'parent-popover'
+    popover.classList.add("tooltip")
     document.body.appendChild(popover);
 }
 
 async function appendingRecordings(word) {
-    let recordings;
-    let popover = document.getElementsByClassName('parent-popover')[0]
     let recordingsDiv = document.createElement('div')
     recordingsDiv.className = 'recordings-div'
-    popover.appendChild(recordingsDiv)
-
-    // New ripped styling!
-    const selectedWordCoordinates = window.getSelection().getRangeAt(0).getBoundingClientRect()
-    const documentCoordinates = document.body.parentNode.getBoundingClientRect();
-    const selectedWordX = selectedWordCoordinates.x
-    const selectedWordY = selectedWordCoordinates.bottom - documentCoordinates.top
-    const selectedWordHeight = selectedWordCoordinates.height
-    const selectedWordWidth = selectedWordCoordinates.width
-    const popoverWidth = 300
-    const popoverHeight = 150
-    const leftRightMargin = 10
-    const bottomTopMargin = 5
-    const arrowNotchHeight = 10
-
-    if (selectedWordX + selectedWordWidth / 2 - Math.ceil(popoverWidth / 2) < 0) { // Appending 'word width' to the conditional check, was because of amplified word styling taking up much space, and skewing the arithmetic wizardy
-        // I could've just substituted the following arithmetic wizardy with "leftRightMargin + 'px'", but I preferred a more 'logically appropriatet' approach
-        popover.style.left = selectedWordX - Math.ceil(popoverWidth / 2) + (0 - (selectedWordX - (popoverWidth / 2))) + leftRightMargin + "px";
-    } else if ((selectedWordX + selectedWordWidth + popoverWidth / 2) > window.innerWidth) {
-        popover.style.left = selectedWordX + selectedWordWidth - Math.ceil(popoverWidth) + "px";
-    } else {
-        popover.style.left = selectedWordX - ((popoverWidth / 2) - (selectedWordWidth / 2)) + "px";
-    }
-
-    if (selectedWordY + popover.offsetHeight >= window.innerHeight) {
-        //popover.style.bottom = "20px";
-        //popover.style.bottom = event.clientY + selectedWordHeight + bottomTopMargin + "px";
-        console.log('Do some shit about it!')
-        popover.style.top = selectedWordY + bottomTopMargin + arrowNotchHeight + "px";
-    } else {
-        popover.style.top = selectedWordY + bottomTopMargin + arrowNotchHeight + "px";
-    }
-
-    const arrowNotchXPosition = (selectedWordX - Number.parseInt(popover.style.left)) + (selectedWordWidth / 2)
 
     // Mainly to hide the scrollbar on chrome & opera & safari
     let popoverStyleElement = document.getElementById('popover-style')
@@ -61,47 +25,22 @@ async function appendingRecordings(word) {
         popoverStyleElement.id = 'popover-style'
         document.body.appendChild(popoverStyleElement)
     }
-    /*
-    * TODO
-    *   - The ability to dynamically allocate the z-index of the popover, so that when I place it just above the selected element, then the user scroll, it can get hidden below other element of higher index
-    */
+    // FIXME: The current popover box doesn't work in certain websites/situations.
+    // FIXME: The z-index of the popover should only exceed the srcElement, to avoid floating other elements i.e., when the user scroll, it can get hidden below other elements
     popoverStyleElement.innerHTML = `
-		div.parent-popover {
-			color: black;
-			position: absolute;
-			background: lightblue;
-			border-radius: 5px;
-			opacity: 0.9;
-			padding: 5px;
-			width: ${popoverWidth}px;
-			height: ${popoverHeight}px;
-			transition: background linear 150ms;
-			z-index: 9999999999;
-		}
-		div.parent-popover::before {
-			content: '';
-			display: block;
-			position: absolute;
-			width: 0;
-			height: 0;
-			border-left: 5px solid transparent;
-			border-right: 5px solid transparent;
-			border-bottom: ${arrowNotchHeight}px solid lightblue;
-			top: -${arrowNotchHeight}px;
-			left: ${arrowNotchXPosition}px;
-		}
 		div.recordings-div {
 			display: flex;
 			text-align: left;
 			flex-direction: column;
 			align-items: flex-start;
 			word-wrap: break-word;
+			position: absolute;
 			width: 100%;
 			height: 100%;
 			overflow: scroll;
 		}
 		div.recordings-div::-webkit-scrollbar { display: none }
-		div.recording-list-item { 
+		div.recording-list-item {
 			display: flex;
 		}
 		button.recording-button {
@@ -115,7 +54,7 @@ async function appendingRecordings(word) {
 			margin-bottom: 2px;
 			padding:2px;
 		}
-		p.recording-name { 
+		p.recording-name {
 			color: black;
 			font-size: small;
 			font-family: sans-serif;
@@ -123,7 +62,7 @@ async function appendingRecordings(word) {
 		}
 	`
 
-    recordings = search(word, await retrieveRecordings(word, 'de', 'en'), {
+    let recordings = search(word, await retrieveRecordings(word, 'de', 'en'), {
         returnMatchData: true,
         keySelector: (obj) => obj.title,
         threshold: 0.0
@@ -142,9 +81,8 @@ async function appendingRecordings(word) {
         recordingListItem.appendChild(buttonRespectiveText)
         recordingsDiv.appendChild(recordingListItem)
     }
-    return (popover)
+    return (recordingsDiv)
 }
-
 
 let audioRecordingsButtons = Array.from(document.getElementsByClassName('recording-button'))
 audioRecordingsButtons.forEach((button) => {
@@ -153,177 +91,88 @@ audioRecordingsButtons.forEach((button) => {
         playAudio(recordingsObject[buttonTextContent][0])
     })
 })
-document.addEventListener('click', function handleButtonclick(event) {
+document.addEventListener('click', (event) => {
     let eventTarget = event.target
     if (eventTarget.className == 'recording-button') {
         playAudio(eventTarget.getAttribute('href'))
     }
 })
 
-let clickTimeoutId;
-document.addEventListener('mouseup', () => {
-    /*
-     *  - The 'mouseup' is the most inclusive event. For instance 'dblclick' won't include dragged to highlight text.
-     *  - The timeout is used to both 
-     *		- Prevent consecutive adding & removing (causes flickering)
-     *      - Different highlighting a word from Long passages
-    */
-    clearTimeout(clickTimeoutId)
-    clickTimeoutId = globalThis.setTimeout(async (event) => {
-        let highlightedValue = window.getSelection().toString().toLowerCase().trim();
-        const specialCharacters = ['.', '?', '!', '"', '\'', '-', '_', '@', '#', '(', ')', '{', '}']
-        specialCharacters.forEach((character) => {
-            if (highlightedValue[0] === character) {
-                highlightedValue = highlightedValue.substring(1)
-            } else if (highlightedValue[highlightedValue.length - 1] === character) {
-                highlightedValue = highlightedValue.substring(highlightedValue.length - 1, 0)
-            }
-        })
-        highlightedValue = highlightedValue.includes('·') ? highlightedValue.replaceAll('·', '') : highlightedValue 		// This special character gets includes sometimes, by google's search, thus eliminating it from the word, before sending the requested word to Forvo's web server.
-        let popoverElements = document.getElementsByClassName('parent-popover')
-        if (!!highlightedValue.length && !popoverElements.length) { // I guess the last condition should be refined with something else (like for example a floating icon being visible or not.. somethig like that)
-            // Appending the popover container element
-            createPopoverContainer();
-            // Append translations
-            appendTranslation(highlightedValue)
-            document.body.appendChild(await appendingRecordings(highlightedValue))
-        }
-    }, 200)
-})
-
-function appendTranslation(highlightedValue) {
-    console.log("More...")
-    console.log(highlightedValue)
-    ;(async () => {
-        let j = document.createElement('div')
-        j.classList.add('recording-list-item')
-        j.classList.add('translation')
-        j.textContent = await parseGoogleTranslateResponse(highlightedValue)
-        document.getElementsByClassName('recordings-div')[0].appendChild(j)
-    })()
+async function appendTranslation(highlightedValue) {
+    const translateElement = document.createElement('div')
+    translateElement.classList.add('recording-list-item')
+    translateElement.classList.add('translation')
+    translateElement.textContent = await parseGoogleTranslateResponse(highlightedValue)
+    return (translateElement)
 }
-
-/*
- * - Two events to handle the automatic removal of the tooltip (for more usability enhancement)
-*/
-function handleToolipRemoval(event) {
-    let popoverElements = document.getElementsByClassName('parent-popover')
-    let eventTargetClassName = event.target.parentNode.className
-    // The scroll behavior is rather a deficit in usability:
-    // 		- According to MY usage, I usually read sequentally, not hopping over the page upside down. Thus I won't be bothered while hopping to encounter a leftover popover hanging out.
-    // 		- having uniform form of removing the popover (i.e. clicking outside the popover) is more standard
-    // 		- The popover gets scrolled past, and thus hidden when:
-    // 			- The popover's inner recordings isn't a list, and the user scrolls for more recordings.
-    // 			- The user scroll all the way to the end of an actual recordings list, then keeps scrolling.
-    if (event.type == 'scroll') {
-        try {
-            let selectedTextY = Math.round(document.getSelection().getRangeAt(0).getBoundingClientRect().y)
-            if (window.scrollY < selectedTextY || window.scrollY > Math.round(selectedTextY)) {
-                //popoverElement.remove()
-            }
-        } catch {
-        }
-    } else if (event.type == 'click') {
-        // Remove the yet-to-be floating box, only if the user clicked in any area but within the box itself
-        if (!['parent-popover', 'recordings-div', 'recording-button', 'recording-list-item', 'recording-list-item translation'].includes(event.target.parentElement.className) && popoverElements.length) {
-            for (let popoverElement of popoverElements) {
-                setTimeout(() => {
-                    popoverElement.remove();
-                }, 0);
-            }
-        }
-    }
-}
-
-['click', 'scroll'].forEach(function (eventElement) {
-    console.log('remvoing the popover')
-    window.addEventListener(eventElement, handleToolipRemoval, false)
-})
 
 /* BUG
  * - [x] Selecting multiple words i.e. phrases, does get a word of the phrase as the chosen one, then it exhibit an astray popover.
  *      - [ ] This bug only occurs when the user double clicks within a paragraph i.e. a word is selected, then the whole paragraph is selected. The initially selected words get an astray popover, while the whole text is now highlighted.
  *      	- It was actually because of something else. It was because of the 'await remote page' delaying the 'highlighted word's respective coordinates styling'. The solution was to move the 'await statement' after the 'popover styling' especially setting its coordinates.
- * - The current popover box doesn't work in certain websites/situations.
  * - The popover box apparently derives its coloring / theming / studying from the respective word/element's parent
  * - Resizing the window i.e. resizing the whole window, opening dev tools, gets the popover astray
- * - hyphenated words don't not detected e.e., ever-growing
+ * - hyphenated words don't get detected e.e., ever-growing
  * - Clicking inside the popover, but outside the click button, would close the popover
  * - CSS Inheritance does take effect sometimes, when you don't explicitly set the CSS property to the popover yourself.
  */
 
-// const selectionRef = document.querySelector('#rcnt')
-// console.log("Hello world")
-// const [instance] = tippy('#rcnt', {
-//   content: 'tooltip',
-//   sticky: true
-// })
-
-// const selection = window.getSelection()
-
-// window.addEventListener('mouseup', (event) => {
-//   if (!selection.isCollapsed) {
-//     const { left, top, width, height } = selection.getRangeAt(0).getBoundingClientRect()
-//     selectionRef.style.left = `${left}px`
-//     selectionRef.style.top = `${top}px`
-//     selectionRef.style.width = `${width}px`
-//     selectionRef.style.height = `${height}px`
-// 
-//     instance.show()
-//   }
-// })
-// window.addEventListener('mousedown', (event) => {
-//   instance.hide()
-// })
-
-
-let selectionReff = document.createElement('style')
-selectionReff.innerHTML = `
-    #selection-ref { 
+createPopoverContainer()
+function onHighlight(event) {
+    const virtualEl = {
+        getBoundingClientRect: () => {
+            const hlBoundingClientRect = document.getSelection().getRangeAt(0).getBoundingClientRect()
+            // console.log(event.clientX, event.clientY, "What")
+            if (hlBoundingClientRect.x <= 0 || hlBoundingClientRect.y <= 0) {
+                return {
+                    width: 0,
+                    height: 0,
+                    x: event.clientX,
+                    y: event.clientY,
+                    top: event.clientY,
+                    left: event.clientX,
+                    right: event.clientX,
+                    bottom: event.clientY,
+                };
+            } else {
+                return hlBoundingClientRect
+            }
+        },
+        getClientRects: () => document.getSelection().getRangeAt(0).getClientRects()
+    };
+    const tooltip = document.querySelector(".tooltip");
+    tooltip.style = `
+        background: #222;
+        width: 250px;
+        height: 150px;
+        color: white;
+        font-weight: bold;
+        padding: 5px;
+        border-radius: 4px;
+        font-size: 90%;
         position: absolute;
+        z-index: 9999999999999999999999999999999999999999999999;
+    `;
+    computePosition(virtualEl, tooltip).then(({x, y}) => {
+        const selectedWord = document.getSelection().toString().toLowerCase().trim()
+        console.log(selectedWord.length)
+        console.log(x, y)
+        Object.assign(tooltip.style, {
+            left: `${x}px`,
+            top: `${y}px`,
+            visibility: (selectedWord.length === 0) ? 'hidden' : 'visible',
+        });
+        if (highlightedWord !== selectedWord) {
+            global.setTimeout(async () => {
+                tooltip.appendChild(await appendTranslation(selectedWord))
+                tooltip.appendChild(await appendingRecordings(selectedWord))
+            }, 0)
+        }
+        if (selectedWord.length === 0 || highlightedWord !== selectedWord) {
+            tooltip.innerHTML = ""
+            highlightedWord = selectedWord
+        }
+    });
+}
 
-        /* debug only styles */
-        background: rgba(200,0,0,0.2);
-        pointer-events: none;
-    }
-`
-document.body.appendChild(selectionReff)
-
-// const selectionRef = document.querySelector('#selection-ref')
-// const [instance] = tippy('#selection-ref', {
-//   content: 'tooltip',
-//   sticky: true
-// })
-
-// inspired by https://jsfiddle.net/joktrpkz/7/
-const selection = window.getSelection()
-window.addEventListener('mouseup', (event) => {
-
-    if (!selection.isCollapsed) {
-        console.log(document.querySelector('#selection-ref'))
-        const {left, top, width, height} = selection.getRangeAt(0).getBoundingClientRect()
-
-        selectionRef.style.left = `${left}px`
-        selectionRef.style.top = `${top}px`
-        selectionRef.style.width = `${width}px`
-        selectionRef.style.height = `${height}px`
-
-        instance.show()
-    }
-})
-
-document.addEventListener("mouseup", (event) => {
-    const selection = window.getSelection();
-    // if (selection && selection.toString()) {
-    console.log(`${selection.toString()} is ...`)
-    let element = document.createElement('div')
-    element.textContent = "More...."
-    const instance = tippy(element)
-    instance.show()
-    instance.popperInstance.reference = document.getElementById("sign-up")
-    // tooltip.popperInstance.reference = selection.getRangeAt(0).getBoundingClientRect();
-    // tooltip.popperInstance.update();
-    // }
-})
-
-const template = document.getElementById('template')
+document.addEventListener('mouseup', onHighlight);
