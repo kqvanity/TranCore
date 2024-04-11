69506d56f0250debc34c4abf39d87380f48d1f9a
mostafa mahmoud <solicitmind@gmail.com>
2024-04-11 10:25:56 +0200

    build: set the content/background source code dirs

--- webpack.config.js
+++ webpack.config.js
@@ -8,29 +8,31 @@ const paths = {
     src: path.resolve(__dirname, 'src')
 }
 
+// TODO: It should be refactored to make use of environment variables or so. [chrome-extension-boilerplate] for reference
+const isDevelopment = true
+
 const options = {
     entry: {
-        content: path.join(__dirname, 'src', 'pages', 'content', 'index.js'),
-        background: path.join(__dirname, 'src', 'pages', 'background', 'index.js'),
+        content: path.join(__dirname, 'src', 'pages', 'content', 'index.ts'),
+        background: path.join(__dirname, 'src', 'pages', 'background', 'index.ts'),
         popup: path.join(__dirname, 'popup.ts')
     },
     mode: 'development',
     // target: "web",
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'build'),
-        clean: true,
-        publicPath: '/'
-    },
     module: {
       rules: [
         {
-          test: /\.(?:js|mjs|cjs)$/,
+          test: /\.(ts|tsx)$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          }
-        }
+          use: [
+              {
+                  loader: "ts-loader",
+                  options: {
+                      transpileOnly: true
+                  }
+              }
+          ]
+        },
       ]
     },
     watch: true,
@@ -56,10 +58,16 @@ const options = {
         })
     ],
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.ts', '.tsx'],
         modules: ['node_modules', path.resolve(__dirname, 'core')],
         symlinks: false
-    }
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+        publicPath: '/'
+    },
 }
 
 module.exports = options
