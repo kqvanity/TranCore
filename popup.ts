5fcad4a8c2323dc82ea5eb2e59fad062fdc4d813
kqvanity <solicitmind@gmail.com>
2024-09-02 12:10:29 +0300

    fix: storybook initially booting

--- popup.ts
+++ popup.ts
@@ -2,10 +2,12 @@ chrome.storage.local.get(["language"], (data) => {
     if (chrome.runtime.lastError) {
         console.error(`Error reading feature value`)
     }
-    document.getElementById("language").value = data["language"]
+    // @ts-ignore
+    document.getElementById("language").textContent = data["language"]
 })
 
 document.getElementById("language")?.addEventListener("change", () => {
+    // @ts-ignore
     const language = document.getElementById("language").value;
     chrome.storage.local.set({ language: language }, () => {
         console.log(`Setting language flag to ${language}`)
@@ -17,10 +19,12 @@ chrome.storage.local.get(["key"], (data) => {
     if (chrome.runtime.lastError) {
         console.error(`Error reading feature value`)
     }
+    // @ts-ignore
     document.getElementById("key").value = data["key"]
 })
 
 document.getElementById("key")?.addEventListener("change", () => {
+    // @ts-ignore
     const key = document.getElementById("key").value;
     chrome.storage.local.set({ key: key }, () => {
         console.log(`Setting key flag to ${key}`)
