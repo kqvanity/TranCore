55fcf9956b74f355ab57a15202322cccf3a3ae30
kqvanity <kqvanity@gmail.com>
2026-01-07 02:40:49 +0200

    refactor(ui): use custom audio player instead of h5audio player

--- src/pages/content/PronPlayer.tsx
+++ src/pages/content/PronPlayer.tsx
@@ -1,34 +1,27 @@
-import AudioPlayer, {RHAP_UI} from "react-h5-audio-player";
-import H5AudioPlayer from "react-h5-audio-player";
-import { Pronunciation } from "pages/content/model";
-import Play from '../../assets/img/play.svg'
-import Pause from '../../assets/img/pause.svg'
+import { Pronunciation } from "./model";
+import { AudioPlayer } from "./AudioPlayer";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    pronPlayer: {
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '10px',
+    },
+    title: {
+        marginLeft: '10px',
+    },
+    translation: {
+        marginLeft: '5px',
+        color: '#666',
+    },
+});
 
 export const PronPlayer = (pron: Pronunciation) => {
-    return <div>
-        <H5AudioPlayer
-            layout={"horizontal"}
-            src={pron.url}
-            customControlsSection={[RHAP_UI.MAIN_CONTROLS]}
-            showJumpControls={false}
-            customProgressBarSection={[]}
-            preload="none"
-            autoPlay={false}
-            autoPlayAfterSrcChange={false}
-            customIcons={{
-                play: <Play />,
-                pause: <Pause />,
-            }}
-            style={{
-                display: "inline-block",
-                width: "0px",
-                paddingRight: "50px",
-                paddingLeft: "10px",
-                paddingBottom: "10px",
-            }}
-        />
-        <span>{pron.title}</span>
-        <span>{' / '}</span>
-        <span>{pron.translation?.title}</span>
+    const classes = useStyles();
+    return <div className={classes.pronPlayer}>
+        <AudioPlayer src={pron.url} />
+        <span className={classes.title}>{pron.title}</span>
+        {pron.translation && <span className={classes.translation}>{' / '}{pron.translation.title}</span>}
     </div>
 }
