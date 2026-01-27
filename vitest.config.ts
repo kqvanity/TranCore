e71ac3716d3279ba9162ff2bb8e6aa677b5b51d1
kqvanity <kqvanity@gmail.com>
2026-01-27 17:11:37 +0200

    build(vite): fix vite relative path

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,12 @@
 import { defineConfig } from 'vitest/config'
 import { mergeConfig } from 'vite'
 import viteConfig from './vite.config'
+import path from 'path'
 
 export default mergeConfig(viteConfig, defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: ['./tests/setup.ts'],
+        setupFiles: [path.resolve(__dirname, './tests/setup.ts')],
     },
 }))
